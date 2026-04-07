c0258d7ae87d1f863a0d4c5d74fdd9378f920d52
tu <hbui@ucc.ie>
2026-04-07 18:15:42 +0100

    run-all run xalpaca gen

--- run-all.sh
+++ run-all.sh
@@ -13,10 +13,10 @@ model_alias=$(basename "$evaluate_model")
 jid1=$(sbatch --output=lm_${model_alias}.log -- run-lm-eval.sh "$evaluate_model" "$output_dir" | awk '{print $4}')
 jid2=$(sbatch --output=mkqa_${model_alias}.log -- run-mkqa.sh "$evaluate_model" "$output_dir" | awk '{print $4}')
 jid3=$(sbatch --output=xlsum_${model_alias}.log -- run-xlsum.sh "$evaluate_model" "$output_dir" | awk '{print $4}')
-jid4=$(sbatch --output=alpaca_${model_alias}.log -- run-alpaca.sh "$evaluate_model" "$output_dir" | awk '{print $4}')
+jid4=$(sbatch --output=alpaca_${model_alias}_gen.log -- run-xalpaca-gen.sh "$evaluate_model"| awk '{print $4}')
 
 echo "Submitted jobs:"
 echo "LM:     $jid1"
 echo "MKQA:   $jid2"
 echo "XLSum:  $jid3"
-echo "Alpaca: $jid4"
+echo "XAlpaca generate: $jid4"
